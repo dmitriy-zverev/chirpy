98826d8f9f56da885573d059a3b7bc7f2395306f
dmitriy-zverev <dimazverev716@gmail.com>
2025-08-30 12:38:24 +0300

    add api config and other handlers

--- main.go
+++ main.go
@@ -3,24 +3,75 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	handler := func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		log.Printf("%d\n", cfg.fileserverHits.Load())
+		next.ServeHTTP(w, req)
+	}
+
+	return http.HandlerFunc(handler)
+}
+
+func (cfg *apiConfig) metricsHandler() http.Handler {
+	handler := func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+
+		numberHits := cfg.fileserverHits.Load()
+		numberHitsStr := strconv.Itoa(int(numberHits))
+		w.Write([]byte("Hits: " + numberHitsStr))
+	}
+	return http.HandlerFunc(handler)
+}
+
+func (cfg *apiConfig) resetHandler() http.Handler {
+	handler := func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		cfg.fileserverHits.Store(0)
+		w.Write([]byte("Hits have been reset"))
+	}
+	return http.HandlerFunc(handler)
+}
+
+func healthzHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
 
 func main() {
 	appPath := "/app/"
 	healthzPath := "/healthz"
+	metricsPath := "/metrics"
+	resetPath := "/reset"
+
+	cfg := apiConfig{}
 
 	serveMux := http.NewServeMux()
 	serveMux.Handle(
 		appPath,
-		http.StripPrefix(
-			appPath,
-			http.FileServer(http.Dir(".")),
+		cfg.middlewareMetricsInc(
+			http.StripPrefix(
+				appPath,
+				http.FileServer(http.Dir(".")),
+			),
 		),
 	)
-	serveMux.HandleFunc(healthzPath, healthz)
+	serveMux.HandleFunc(healthzPath, healthzHandler)
+	serveMux.HandleFunc(metricsPath, cfg.metricsHandler().ServeHTTP)
+	serveMux.HandleFunc(resetPath, cfg.resetHandler().ServeHTTP)
 
 	port := "8080"
-
 	server := &http.Server{
 		Handler: serveMux,
 		Addr:    ":" + port,
@@ -28,10 +79,4 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", appPath, port)
 	log.Fatal(server.ListenAndServe())
-}
-
-func healthz(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
 }
