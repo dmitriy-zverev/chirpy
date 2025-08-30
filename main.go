29bb5f2bd0afc796ebcd25cfd4b6906a2f029763
dmitriy-zverev <dimazverev716@gmail.com>
2025-08-30 19:08:49 +0300

    add validate_chirp logic to send responses in json format

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"slices"
 	"strconv"
+	"strings"
 	"sync/atomic"
 )
 
@@ -64,6 +67,87 @@ func middlewareLog(next http.Handler) http.Handler {
 	})
 }
 
+func validateChirpHandler(w http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	if err := decoder.Decode(&params); err != nil {
+		log.Printf("Error decoding parameters: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+
+		type returnVals struct {
+			Error string `json:"error"`
+		}
+
+		respBody := returnVals{
+			Error: "Something went wrong",
+		}
+		dat, err := json.Marshal(respBody)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(dat)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		type returnVals struct {
+			Error string `json:"error"`
+		}
+
+		respBody := returnVals{
+			Error: "Chirp is too long",
+		}
+		dat, err := json.Marshal(respBody)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(dat)
+		return
+	}
+
+	type returnVals struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	splittedBody := strings.Split(params.Body, " ")
+	profoundWords := []string{
+		"kerfuffle",
+		"sharbert",
+		"fornax",
+	}
+
+	for i, word := range splittedBody {
+		if slices.Contains(profoundWords, strings.ToLower(word)) {
+			splittedBody[i] = "****"
+		}
+	}
+
+	respBody := returnVals{
+		CleanedBody: strings.Join(splittedBody, " "),
+	}
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(dat)
+}
+
 func main() {
 	appPrefix := "/app/"
 	apiPrefix := "/api"
@@ -72,6 +156,7 @@ func main() {
 	healthzPath := apiPrefix + "/healthz"
 	metricsPath := adminPrefix + "/metrics"
 	resetPath := adminPrefix + "/reset"
+	validateChirpPath := apiPrefix + "/validate_chirp"
 
 	cfg := apiConfig{}
 
@@ -88,6 +173,7 @@ func main() {
 	serveMux.HandleFunc("GET "+healthzPath, healthzHandler)
 	serveMux.HandleFunc("GET "+metricsPath, cfg.metricsHandler().ServeHTTP)
 	serveMux.HandleFunc("POST "+resetPath, cfg.resetHandler().ServeHTTP)
+	serveMux.HandleFunc("POST "+validateChirpPath, validateChirpHandler)
 
 	port := "8080"
 	server := &http.Server{
